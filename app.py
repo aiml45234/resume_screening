aef3170417dcc963b81b475d2379e3d02b27c520
aiml45234 <159514759+aiml45234@users.noreply.github.com>
2024-02-12 14:40:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def set_main_style(side_bg):
           background-position: right center;
           background-size: 100vw 100vh;
           background-size: cover;
-          background-attachment: initial;
+          background-attachment: fixed;
           
       }}
       [data-testid="stHeader"] {{
