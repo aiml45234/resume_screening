8b6afd1914c9196f86a98db0fa73a6a055e0eaf8
aiml45234 <159514759+aiml45234@users.noreply.github.com>
2024-02-12 10:55:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,8 @@ def set_sidebar_style(side_bg):
       <style>
       [data-testid="stSidebar"] > div:first-child {{
           background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
+          background-position: center;
+          background-size: cover;
       }}
       </style>
       """,
