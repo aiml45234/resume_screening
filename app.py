8349acdea4e47f5ef4f8df9030617fc334bfb53d
aiml45234 <159514759+aiml45234@users.noreply.github.com>
2024-02-12 14:15:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,17 @@ import time
 st.set_page_config(page_title="Resume Screening Helper")
 uploaded_file_path = ""
 rest_flag = False
+# Custom CSS to change text color to white in dark mode
+custom_css = """
+<style>
+body.darkMode {
+    color: white;
+}
+</style>
+"""
 
+# Inject custom CSS into Streamlit app
+st.markdown(custom_css, unsafe_allow_html=True)
 
 if 'session_id' not in st.session_state:
     st.session_state['session_id'] =''
